33f87d9133012567eb656cf84a32997ee22c5007
kyunggwan <4562sky@naver.com>
2023-05-01 13:40:41 +0900

    feat: boardDto 작성

--- basic/board/src/main/java/com/board/dto/BoardDto.java
+++ basic/board/src/main/java/com/board/dto/BoardDto.java
@@ -1,4 +1,20 @@
 package com.board.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class BoardDto {
+
+    private long id;
+    private String boardTitle;
+    private String boardContent;
+    private String boardWriter;
+    private String boardCreateTime;
+
 }
