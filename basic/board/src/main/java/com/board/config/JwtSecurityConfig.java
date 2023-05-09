d1497dd10a4b053713ed8e02a72d4e0824de29b0
kyunggwan <4562sky@naver.com>
2023-05-09 15:05:08 +0900

    docs: 주석 수정

--- basic/board/src/main/java/com/board/config/JwtSecurityConfig.java
+++ basic/board/src/main/java/com/board/config/JwtSecurityConfig.java
@@ -8,12 +8,13 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-// 직접 만든 TokenProvider 와 JwtFilter 를 SecurityConfig 에 적용할 때 사용
+// JwtFilter 를 SecurityConfig 에 적용
 @RequiredArgsConstructor
 public class JwtSecurityConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
     private final TokenProvider tokenProvider;
 
-    // TokenProvider 를 주입받아서 JwtFilter 를 통해 Security 로직에 필터를 등록
+    // JwtFilter(토큰이 유효한지를 확인하는 필터)에 TokenProvier정보를 주입해서 필터를 만듦
+    // Security 로직에 필터를 등록: SpringSrcurity 전반적인 필터에 적용이 된다.
     @Override
     public void configure(HttpSecurity http) {
         JwtFilter customFilter = new JwtFilter(tokenProvider);
