f990a9c160ad6baa8c6ee161f67659334dc354bd
kyunggwan <4562sky@naver.com>
2023-05-10 11:21:06 +0900

    chore: 모듈 설치 및 기타 세팅

--- typescript/src/Main/Home/Home.tsx
+++ typescript/src/Main/Home/Home.tsx
@@ -1,7 +1,26 @@
 import "./index.css";
+import { useTypewriter, Cursor } from "react-simple-typewriter";
 
-export default function Home() {
+type Props = {};
+export default function Home(props: Props) {
+
+   const [text, count] = useTypewriter({
+     words: [
+       " 홈 화면 입니다.",
+       " 여러 라이브러리를 적용해보고 있습니다.",
+       " 이거 재밌습니다."
+     ],
+     loop: true,
+     delaySpeed: 3000,
+   });
+
   return (
-<>
-    <h1>Home 화면입니다</h1>
-</>  )}
+    <>
+      <h1>Home 화면입니다</h1>
+      <p>
+        {text}
+        <Cursor cursorColor="#c54e7e" cursorStyle="|" cursorBlinking={true} />
+      </p>
+    </>
+  );
+}
--- typescript/src/pages/Profile/Profile.tsx
+++ typescript/src/pages/Profile/Profile.tsx
@@ -1,6 +1,8 @@
 import Authentication from '../Authentication/Authentication';
 import ProfileList from './ProfileList/ProfileList';
 import { useUserStore } from '../../stores';
+import { Typography } from 'antd';
+const { Text } = Typography;
 
 export default function Profile() {
 const { user } = useUserStore();
@@ -8,7 +10,6 @@ const { user } = useUserStore();
 
   return (
     <div>
-      profile 로그인을 해야 볼 수 있는 게시판이다. 로그인이 되면 회원 목록을, 로그인이 안되면 로그인 창을 보여준다
       {user ? <ProfileList /> : <Authentication />}
     </div>
   );
