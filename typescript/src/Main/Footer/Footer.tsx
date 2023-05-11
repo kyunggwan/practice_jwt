98b5d6e5e982821728410043d26b11012824aaa5
kyunggwan <4562sky@naver.com>
2023-05-11 23:02:08 +0900

    refactor: footer에 props 중복 삭제

--- typescript/src/Main/Footer/Footer.tsx
+++ typescript/src/Main/Footer/Footer.tsx
@@ -2,23 +2,22 @@ import { useState, useEffect } from "react";
 import { Menu, Typography } from "antd";
 import "./index.css";
 import type { MenuTheme } from "antd";
-interface HeaderProps {
+
+interface FooterProps {
   darkMode: boolean;
 }
 
-export default function Footer(props: HeaderProps) {
-  const { darkMode } = props;
+export default function Footer({ darkMode }: FooterProps) {
   const [theme, setTheme] = useState<MenuTheme>("dark");
 
-  // footer 다크모드 설정
- useEffect(() => {
-   setTheme(darkMode ? "dark" : "light");
- }, [darkMode]);
+  useEffect(() => {
+    setTheme(darkMode ? "dark" : "light");
+  }, [darkMode]);
 
   return (
     <Menu className="Footer" theme={theme}>
       <Typography.Link href="tel:+123456789">+12345678</Typography.Link>
-      <Typography.Link href="http://www.google.com" target={"_blank"}>
+      <Typography.Link href="http://www.google.com" target="_blank">
         Privacy Policy
       </Typography.Link>
       <Typography.Link href="http://www.google.com">
