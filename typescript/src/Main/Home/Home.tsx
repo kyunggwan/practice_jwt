1ef3228537cc2f1cbac2424063c5f1af1a658889
kyunggwan <4562sky@naver.com>
2023-05-11 09:35:58 +0900

    feat: 홈화면 동영상 삽입, css 수정필요

--- typescript/src/Main/Home/Home.tsx
+++ typescript/src/Main/Home/Home.tsx
@@ -1,23 +1,36 @@
 import "./index.css";
 import { useTypewriter, Cursor } from "react-simple-typewriter";
+import ReactPlayer from "react-player";
+import './index.css';
 
 type Props = {};
 export default function Home(props: Props) {
 
    const [text, count] = useTypewriter({
      words: [
-       " 홈 화면 입니다.",
+       " 샘플 게시판 홈 화면입니다.",
        " 여러 라이브러리를 적용해보고 있습니다.",
        " 이거 재밌습니다."
      ],
      loop: true,
-     delaySpeed: 1000,
+     delaySpeed: 3000,
    });
 
   return (
     <>
       <h1>Home 화면입니다</h1>
-      <p>
+      <div className="homeVideo">
+        <ReactPlayer
+          url="https://www.youtube.com/watch?v=l-B1_h_TQHI"
+          width="100%"
+          height="100%"
+          loop={true}
+          playing={true}
+          muted={true}
+          controls={false}
+        />
+      </div>
+      <p className="homeText" >
         {text}
         <Cursor cursorColor="#c54e7e" cursorStyle="|" cursorBlinking={true} />
       </p>
