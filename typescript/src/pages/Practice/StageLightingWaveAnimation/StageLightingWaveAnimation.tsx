1a19d60d2ea2eb6980010bb8b20565bbfe97874b
kyunggwan <4562sky@naver.com>
2023-05-16 18:27:10 +0900

    feat: 0516 작업

--- typescript/src/pages/Practice/StageLightingWaveAnimation/StageLightingWaveAnimation.tsx
+++ typescript/src/pages/Practice/StageLightingWaveAnimation/StageLightingWaveAnimation.tsx
@@ -1,7 +1,12 @@
-import {useRef} from 'react'
+import {useRef, RefObject, useEffect} from 'react'
 
 export default function StageLightingWaveAnimation () {
-    const canvasRef = useRef<HTMLCanvasElement>(null);
+    const canvasRef: RefObject<HTMLCanvasElement> = useRef<HTMLCanvasElement>(null);
+
+    useEffect(() => {
+        console.log('3 useEffect');
+    })
+
   return (
     <div>
         <canvas ref={canvasRef} />
